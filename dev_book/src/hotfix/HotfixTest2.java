f2497486dc0c3e6bddfe8052c5c1472f703a5b56
onil11 <onil11@naver.com>
2020-03-23 16:17:07 +0900

    바보

--- dev_book/src/hotfix/HotfixTest2.java
+++ dev_book/src/hotfix/HotfixTest2.java
@@ -3,7 +3,7 @@ package hotfix;
 public class HotfixTest2 {
 
 	public static void main(String[] args) {
-	//프로젝트명 -> 오른쪽 -> Remote -> Switch to -> new Branch -> hotfix
-		System.out.println("hotfix 브랜치에서 한번 더 추가한 소스임.");
+	//hotfix브랜치에서 저장점을 한개 더 가지기 위한 소스 추가.
+		System.out.println("hotfix 브랜치에서 2번째로 추가한 소스임.");
 	}
 }
