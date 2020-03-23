dde9f25e7a42a62cc3ee51d54981a25d7b53a631
onil11 <onil11@naver.com>
2020-03-23 15:56:55 +0900

    hotfix브랜치에서 저장점을 한개 더 가지기 위한 소스 추가.

---
+++ dev_book/src/hotfix/HotfixTest2.java
@@ -0,0 +1,9 @@
+package hotfix;
+
+public class HotfixTest2 {
+
+	public static void main(String[] args) {
+	//프로젝트명 -> 오른쪽 -> Remote -> Switch to -> new Branch -> hotfix
+		System.out.println("hotfix 브랜치에서 한번 더 추가한 소스임.");
+	}
+}
